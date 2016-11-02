d82d63ea89f92fa7563bec53a054fdac6cf1235f
Abhay Rana <me@captnemo.in>
2016-11-02 07:23:32 +0530

    Adds a few more negative tests

--- tests/node/index.js
+++ tests/node/index.js
@@ -5,4 +5,7 @@ var assert = require('assert');
 assert.equal(true, ifsc.validate('JAKA0AALLAH'));
 assert.equal(true, ifsc.validate('KARB0000214'));
 assert.equal(false, ifsc.validate('KARB0000226'));
+assert.equal(false, ifsc.validate('NEMO0000226'));
+assert.equal(false, ifsc.validate('KARB1000214'));
+assert.equal(false, ifsc.validate('KARBX000214'));
 
