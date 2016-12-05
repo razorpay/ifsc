6c4fa471ad7bb2055c802fb71e09251404fe5bb9
Abhay Rana <me@captnemo.in>
2016-12-05 18:03:30 +0530

    Adds tests for Bank Codes

---
+++ tests/BankNameTest.php
@@ -0,0 +1,16 @@
+<?php
+namespace Razorpay\IFSC\Tests;
+
+use Razorpay\IFSC\Bank;
+use PHPUnit_Framework_TestCase;
+
+class Name extends PHPUnit_Framework_TestCase
+{
+	public function testDefined()
+	{
+		$this->assertEquals('PUNB', constant('Razorpay\IFSC\Bank::PUNB'));
+		$this->assertEquals('BDBL', constant('Razorpay\IFSC\Bank::BDBL'));
+
+		$this->assertEquals('BDBL', Bank::BDBL);
+	}
+}
