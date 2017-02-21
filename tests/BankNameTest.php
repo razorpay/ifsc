c12402e7b13e8460b5c0a60b01800ff52b5bd5cc
Abhay Rana <me@captnemo.in>
2017-02-21 21:15:31 +0530

    [style] Fixes indentation to 4 spaces

--- tests/BankNameTest.php
+++ tests/BankNameTest.php
@@ -6,11 +6,11 @@ use PHPUnit_Framework_TestCase;
 
 class Name extends PHPUnit_Framework_TestCase
 {
-	public function testDefined()
-	{
-		$this->assertEquals('PUNB', constant('Razorpay\IFSC\Bank::PUNB'));
-		$this->assertEquals('BDBL', constant('Razorpay\IFSC\Bank::BDBL'));
+    public function testDefined()
+    {
+        $this->assertEquals('PUNB', constant('Razorpay\IFSC\Bank::PUNB'));
+        $this->assertEquals('BDBL', constant('Razorpay\IFSC\Bank::BDBL'));
 
-		$this->assertEquals('BDBL', Bank::BDBL);
-	}
+        $this->assertEquals('BDBL', Bank::BDBL);
+    }
 }
