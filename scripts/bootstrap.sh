701c7adcd6146003b71ec73f623460d95e5d6137
Nemo <me@captnemo.in>
2017-03-01 20:14:31 +0530

    Shifts to using bundle exec

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -3,10 +3,10 @@ set -euo pipefail
 IFS=$'\n\t'
 
 wget "https://www.rbi.org.in/Scripts/bs_viewcontent.aspx?Id=2009" -O list.html -o /dev/null
-ruby parse_list.rb > excel_list.txt
+bundle exec ruby parse_list.rb > excel_list.txt
 rm -rf sheets
 mkdir -p sheets
 wget -i excel_list.txt -P sheets/
 mkdir -p data/by-bank
 # This is the script that does all the data generation
-ruby generate.rb
+bundle exec ruby generate.rb
