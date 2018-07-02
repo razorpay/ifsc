acf9208f5e5b0d20f206953f47d48f90a5a0ea4a
Nemo <nemo@razorpay.com>
2018-07-02 11:35:52 +0530

    [deps] Version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule IFSC.Mixfile do
   def project do
     [
       app: :ifsc,
-      version: "1.1.5",
+      version: "1.1.6",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       description: description(),
