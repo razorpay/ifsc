af57c721d6837b54ad7273ab3a84331773695a9a
Nemo <nemo@razorpay.com>
2019-04-27 02:35:50 +0530

    [node] Style fixes and API upgrade

--- tests/node/validator_test.js
+++ tests/node/validator_test.js
@@ -1,15 +1,15 @@
-var ifsc = require('./../../src/node/index');
-var assertions = require('../validator_asserts.json');
+var ifsc = require("./../../src/node/index");
+var assertions = require("../validator_asserts.json");
 
-var assert = require('assert');
+var assert = require("assert");
 
 for (var testLabel in assertions) {
-	var group = assertions[testLabel];
-	assertGroup(testLabel, group);
+  var group = assertions[testLabel];
+  assertGroup(testLabel, group);
 }
 
 function assertGroup(message, group) {
-	for (var code in group) {
-		assert.equal(group[code], ifsc.validate(code), message);
-	}
+  for (var code in group) {
+    assert.equal(group[code], ifsc.validate(code), message);
+  }
 }
