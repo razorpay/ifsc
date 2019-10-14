9cc68d86349b90596d42087d5de953c16bc23792
Nemo <me@captnemo.in>
2019-10-14 13:59:17 +0530

    Fix RBI file path

--- scraper/scripts/bootstrap.sh
+++ scraper/scripts/bootstrap.sh
@@ -6,7 +6,7 @@ mkdir --parents data/by-bank sheets
 
 # List of sublet branches, and IMPS only branches
 wget "https://www.npci.org.in/national-automated-clearing-live-members-1" --output-document=nach.html --user-agent="Firefox"
-wget --timestamping --no-verbose --directory-prefix=sheets/ "https://rbidocs.rbi.org.in/rdocs/content/docs/68774.xls" || true
+wget --timestamping --no-verbose --directory-prefix=sheets/ "https://rbidocs.rbi.org.in/rdocs/content/docs/68774.xlsx" || true
 wget --timestamping --no-verbose --directory-prefix=sheets/ "https://rbidocs.rbi.org.in/rdocs/RTGS/DOCs/RTGEB0815.xlsx" || true
 
 echo "Sheet Download complete, starting export"
