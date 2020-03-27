3e7ec6eea5bd993334f8a4c49e3c22bd393ab303
Nemo <nemo@razorpay.com>
2020-03-27 15:05:24 +0530

    Improve release notes generator to avoid common IFSCs

--- scraper/scripts/release.sh
+++ scraper/scripts/release.sh
@@ -2,7 +2,7 @@
 
 # Generate a Release Changelog
 # 1. Clone the latest ifsc-api repo
-git clone https://github.com/razorpay/ifsc-api.git --depth 1
+# git clone https://github.com/razorpay/ifsc-api.git --depth 1
 # 2. Extract the latest files
 (cd data && tar -xzvf by-bank.tar.gz)
 # 3. Copy the data files to the repo
@@ -10,7 +10,7 @@ cp data/by-bank/* ifsc-api/data/
 pushd ifsc-api
 git add data/
 # Generate the complete diff
-git diff --staged -U0 |grep '"IFSC": "' |awk '{print $1substr($3,2,11)}'|sort -u > diff.txt
+git diff --ignore-all-space --staged -U0 |grep '"IFSC": "' |awk '{print $1substr($3,2,11)}'|sort -u > diff.txt
 # Generate the summarized diff
 cat diff.txt | cut -c-5 | sort |uniq -c | sort -n > diffsummary.txt
 popd
--- scraper/scripts/releasenotes.php
+++ scraper/scripts/releasenotes.php
@@ -1,8 +1,28 @@
 **Release Date**: `TODO`
 **RBI Update Date**: `TODO`
 <?php
+// Do some pre-processing
+$plus = $minus = [];
+foreach(file('ifsc-api/diff.txt') as $row) {
+    $ifsc = substr($row, 1, -1);
+    if (substr($row,0,1) === '+') {
+        $plus[] = $ifsc;
+    }
+    elseif (substr($row, 0, 1) === '-') {
+        $minus[] = $ifsc;
+    }
+}
+
+$common = array_intersect($plus, $minus);
+$plus = array_diff($plus, $common);
+$minus = array_diff($minus, $common);
+
+sort($plus);sort($minus);
+
+$diffSize = count($plus)+count($minus);
+
+// Reduce one for the final newline
 $ifscCount = (((int) `wc -l data/IFSC.csv`) - 1);
-$diffSize = (((int) `wc -l ifsc-api/diff.txt`) - 1);
 ?>**IFSC Count**: <?=$ifscCount;?>
 
 **Diff Size**: <?=$diffSize?> (This only counts new or deleted IFSCs from previous release)
@@ -21,7 +41,14 @@ $diffSize = (((int) `wc -l ifsc-api/diff.txt`) - 1);
 <details><summary><strong>Exact IFSC Diff</strong></summary>
 
 ```diff
-<?=file_get_contents('ifsc-api/diff.txt');?>
+<?php
+foreach ($plus as $ifsc) {
+    echo "+$ifsc\n";
+}
+foreach ($minus as $ifsc) {
+    echo "-$ifsc\n";
+}
+?>
 ```
 </details>
 
