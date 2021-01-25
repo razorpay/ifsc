63b71b5d24f3242b394112ec8eb0a793290ca0cf
Nemo <nemo@razorpay.com>
2021-01-25 17:47:59 +0530

    [ruby] Fix SBI website SWIFT fetch code

--- scraper/scripts/methods.rb
+++ scraper/scripts/methods.rb
@@ -331,7 +331,7 @@ end
 # Downloads the SWIFT data from
 # https://sbi.co.in/web/nri/quick-links/swift-codes
 def validate_sbi_swift
-  doc = Nokogiri::HTML(open("https://sbi.co.in/web/nri/quick-links/swift-codes"))
+  doc = Nokogiri::HTML(URI.open("https://sbi.co.in/web/nri/quick-links/swift-codes"))
   table = doc.css('tbody')[0]
   website_bics = Set.new
 
