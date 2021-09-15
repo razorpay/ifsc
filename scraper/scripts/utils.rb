27ff0123aef344a9186941fe173d78ed4c361a72
Nemo <me@captnemo.in>
2021-09-15 16:56:11 +0530

    Parser improvements
    - Try to guess the correct state when incorrect
    - Improve phone number parsing
    - One row has a pipe-delimited format, parse that correctly

--- scraper/scripts/utils.rb
+++ scraper/scripts/utils.rb
@@ -19,10 +19,23 @@ end
 # Some rows have last 2 columns shifted by 2
 # Check for numeric values of STATE in RTGEB0815.xlsx for examples
 # This checks and fixes those
-def fix_row_alignment_for_rtgs(row)
-  log "#{row['IFSC']}: Switching State(#{row['STATE']}) and ADDRESS(#{row['ADDRESS']})", :info
-  row['STATE'], row['ADDRESS'] = row['ADDRESS'], row['STATE']
-  return row
+def fix_row_alignment!(row)
+  log "#{row['IFSC']}: Using State = '#{row['CITY2']}' STD_CODE=#{row['STATE']}, PHONE=#{row['STD CODE']} and discarding PHONE=#{row['PHONE']}", :info
+  row['STATE'],row['STD CODE'], row['PHONE'] = row['CITY2'], row['STATE'], row['STD CODE']
+end
+
+def fix_pipe_delimited_address!(row)
+  log "Splitting address= #{row['ADDRESS']}. New values=", :info
+
+  d = row['ADDRESS'].split '|'
+
+  row['PHONE'] =  d[-1]
+  row['STD CODE'] =  d[-2]
+  row['STATE'] =  d[-3]
+  row['CITY2'] =  d[-4]
+  row['CITY1'] =  d[-5]
+  row['ADDRESS'] =  d[-6]
+  log row.select{|k,v| ['ADDRESS','PHONE', 'STD CODE', 'STATE', 'CITY1', 'CITY2'].include? k}, :info
 end
 
 def bank_name_from_code(code)
